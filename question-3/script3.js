2b487d59456d0fe09ac68930c6f99a5e0f9b9555
claudiacut <claudiamaria.cutrupi@mail.polimi.it>
2020-11-16 17:51:00 +0100

    THUMB VIZ

---
+++ question-3/script3.js
@@ -0,0 +1,13 @@
+Promise.all([d3.html("./question.html"), d3.html("./viz3.svg")]).then(function([html, svgDocument]){
+const questionContent = d3.select(html).selectAll('body > *');
+questionContent.each(function(d){
+d3.select("#question-container").node().appendChild(this);
+});
+let svgNode = svgDocument.querySelector("svg");
+// console.log(svgNode);
+let container = document.querySelector("#visualization");
+// console.log(container);
+container.appendChild(svgNode);
+
+  
+});
