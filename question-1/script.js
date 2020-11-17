5d00445ebde330a4f519718c85bd94ec722dcd9e
claudiacut <claudiamaria.cutrupi@mail.polimi.it>
2020-11-17 16:55:43 +0100

    q1

--- question-1/script.js
+++ question-1/script.js
@@ -10,16 +10,27 @@ let container = document.querySelector("#visualization");
 container.appendChild(svgNode);
 
   const clusters = d3.selectAll("#nodes > g");
+  const fake = d3.selectAll("#fake");
 
-  clusters.on("mouseover", function(){
+  clusters.on("click", function(){
     // console.log(this);
-    clusters.style("opacity","0.1");
-
-    d3.select(this).style("opacity","1");
+    clusters.style("opacity","0");
+    fake.transition().duration(1).ease(d3.easeLinear).style("opacity","0.7");
+    d3.select(this).transition().delay(1).ease(d3.easeLinear).style("opacity","1");
   });
 
-  d3.select("#sfondo").on("mouseover",function(){
-    clusters.style("opacity","1");
+  d3.select("#sfondo").on("click",function(){
+    clusters.transition().duration(2).ease(d3.easeLinear).style("opacity","1");
   });
-
+//
+//    const prova = d3.selectAll("#songs1 > g");
+//    const pro = d3.selectAll("#kids > g");
+//    const songs = d3.selectAll("#song > g");
+//    prova.on("click", function(){
+//        pro.style("opacity","0");
+//        songs.style("opacity","1");
+//        d3.select(this).style("opacity","1");
+//        
+//    });
+   
 });
