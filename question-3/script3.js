820524b611bf82202aaf2e1f8fb4f9e09cc9486a
Isabella Possaghi <isabella.possaghi@gmail.polimi.it>
2020-11-18 23:39:15 +0100

    viz_3

--- question-3/script3.js
+++ question-3/script3.js
@@ -1,4 +1,4 @@
-Promise.all([d3.html("./question.html"), d3.html("./720.svg")]).then(function([html, svgDocument]){
+Promise.all([d3.html("./question.html"), d3.html("./viz3.svg")]).then(function([html, svgDocument]){
 const questionContent = d3.select(html).selectAll('body > *');
 questionContent.each(function(d){
 d3.select("#question-container").node().appendChild(this);
@@ -9,5 +9,29 @@ let container = document.querySelector("#visualization");
 // console.log(container);
 container.appendChild(svgNode);
 
-  
+
+
+const _2016 = d3.selectAll('#_2016 > g');
+const t31 = d3.selectAll("#t31 > g");
+
+console.log(ciano);
+
+
+t31.on('mouseover', function(){
+_2016.style('opacity',0.0);
+// «this» refers to hovering element
+  d3.select(this).transition().duration(200).ease(d3.easeLinear).style('opacity',1);
+});
+
+
+
+
+// bind a hover event to the rectangle in the background
+// to reset selection
+// use mouseover for the hover selection
+d3.select('#sfondo').on('mouseover', function(){
+  _2016.transition().duration(2).ease(d3.easeLinear).style('opacity',1);
+});
+
+
 });
