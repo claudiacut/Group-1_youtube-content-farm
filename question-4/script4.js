aaab7da2c05a0fc37baf0c90b9572d0adaebc8e5
Isabella Possaghi <isabella.possaghi@gmail.polimi.it>
2020-11-18 18:08:26 +0100

    viz_4

--- question-4/script4.js
+++ question-4/script4.js
@@ -13,41 +13,22 @@ Promise.all([d3.html("./question.html"), d3.html("./viz4.svg")]).then(function([
   // const clusters = d3.selectAll('#nodes > g');
   //non metto > g perchè non ci sono sottolovelli
   const palline = d3.selectAll('#palle > g');
-  const fake = d3.selectAll('#fake > g');
-  const liv114 = d3.select('#liv114');
-  const img114 = d3.select('#img114');
-
-  img114.style('opacity', 0);
+  const img = d3.selectAll('#fake');
+  const liv1 = d3.select('#liv114');
+  const img1 = d3.select('#img114');
 
 
+  img1.style('opacity', 0);
 
-
-
-
-
-
-
-  // palline.on('mouseover', function() {
-  //   palline.transition().duration(1).ease(d3.easeLinear).style('opacity', 0.05);
-  //   d3.select(this).transition().duration(200).ease(d3.easeLinear).style('opacity', 1);
-  //   liv114.transition().duration(200).ease(d3.easeLinear).style('opacity', 1);
-  // });
-
-
-  liv114.on('mouseover', function() {
+    liv1.on('mouseover', function() {
     palline.transition().duration(1).ease(d3.easeLinear).style('opacity', 0.05);
     d3.select(this).transition().duration(200).ease(d3.easeLinear).style('opacity', 1);
-    img114.transition().duration(200).ease(d3.easeLinear).style('opacity', 1);
-
-
+    img1.transition().duration(200).ease(d3.easeLinear).style('opacity', 1);
   });
-//creare classe per i related
-//quando c'è over per uno, far scomparire gli altri related
-
 
   d3.select('#sfondo').on('mouseover', function(){
     palline.transition().duration(2).ease(d3.easeLinear).style('opacity',1);
-    img114.transition().duration(200).ease(d3.easeLinear).style('opacity', 0);
+    img1.transition().duration(200).ease(d3.easeLinear).style('opacity', 0);
   });
 
 });
