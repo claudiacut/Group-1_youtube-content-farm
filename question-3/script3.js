595d92fb7d455e03061a730f884fce728060fcc0
Isabella Possaghi <isabella.possaghi@gmail.polimi.it>
2020-12-03 12:09:23 +0100

    VIZ 3 FINITA FINALLY HELP HO PERSO DUE GIORNI

--- question-3/script3.js
+++ question-3/script3.js
@@ -13,7 +13,7 @@ Promise.all([d3.html("./question.html"), d3.html("./viz3.svg")]).then(function([
     var vizz=null;
   const anni = d3.selectAll("#anni").selectAll("g");
   const img = d3.selectAll("#anni").selectAll("g").selectAll("image");
-  const ombre = d3.selectAll("#anni").selectAll("g").selectAll(".ombra");
+  const ombre = d3.selectAll("#anni").selectAll("g").selectAll("#ombra");
 
 
 
@@ -31,7 +31,7 @@ Promise.all([d3.html("./question.html"), d3.html("./viz3.svg")]).then(function([
 
         d3.select(this).selectAll("image").transition().duration(80).ease(d3.easeLinear).style("opacity", 1);
         // d3.select(this).selectAll("rect").style("opacity", 1);
-        d3.select(this).selectAll(".ombra").style("opacity",1);
+        d3.select(this).selectAll("#ombra").style("opacity",1);
 
 
           });
